f8c8d87f7cd0da9bcda0f15f6ed33c3caf2e0799
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-07 07:50:51 -0500

    Script

---
+++ DiegoGarzon.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+
+#PBS -l nodes=1;intel:ppn=8, mem=64gb
+#PBS -l walltime=00:05:00
+#PBS -M da.garzon1@uniandes.edu.co
+#PBS -m abe
+#PBS -N DiegoGarzon_Ejercicio25
+#PBS -j oe
+
+module load anaconda/python3
+cd da.garzon1/DiegoGarzon_Ejercicio25
+make
